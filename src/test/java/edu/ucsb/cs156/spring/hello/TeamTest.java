c3eafe8bb1d849e619ad9acea4447fdf293ada52
Dongyi Xia <dongyi_xia@ucsb.edu>
2026-04-09 18:16:20 -0700

    finish jpa02

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -1,9 +1,13 @@
 package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.ArrayList;
 
 public class TeamTest {
 
@@ -19,8 +23,66 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void equals_Case_1() {
+        assertTrue(team.equals(team));
+    }
+
+    @Test
+    public void equals_Case_2() {
+        int a = 8;
+        assertFalse(team.equals(a));
+    }
+
+    @Test
+    public void equals_Case_3_TT() {
+        Team team_test1 = new Team("test-team");
+        assertTrue(team.equals(team_test1));
+    }
+
+    @Test
+    public void equals_Case_3_TF() {
+        Team team_test1 = new Team("test-team");
+        team_test1.setMembers(new ArrayList<>(Arrays.asList("Alice", "Bob")));
+        assertFalse(team.equals(team_test1));
+    }
+
+    @Test
+    public void equals_Case_3_FT() {
+        Team team_test1 = new Team("Hawaii");
+        assertFalse(team.equals(team_test1));
+    }
+
+    @Test
+    public void equals_Case_3_FF() {
+        Team team_test1 = new Team("Hawaii");
+        team_test1.setMembers(new ArrayList<>(Arrays.asList("Alice", "Bob")));
+        assertFalse(team.equals(team_test1));
+    }
+
+    @Test
+    public void hashCode_equals() {
+        Team t1 = new Team();
+        t1.setName("foo");
+        t1.addMember("bar");
+        Team t2 = new Team();
+        t2.setName("foo");
+        t2.addMember("bar");
+        assertEquals(t1.hashCode(), t2.hashCode());
+    }
+
+    @Test
+    public void hashCode_returns_correct_hasahCode() {
+        int result = team.hashCode();
+        int expectedResult = -1226298695;
+        assertEquals(expectedResult, result);
+    }
    
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+
 
 }
